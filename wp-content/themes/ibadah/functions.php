35c61878db0f7498c8d35b8e2b7a276f94dbf905
Shazzad Hossen <hossensajjadaitvet@gmail.com>
2024-07-26 14:28:46 +0600

    modify function

--- wp-content/themes/ibadah/functions.php
+++ wp-content/themes/ibadah/functions.php
@@ -4,16 +4,24 @@
  */
 
 //Theme Title 
-add_theme_support('title-tag');
+add_theme_support('title-tag'); 
 
 // Theme CSS JS File CALLING ---------------------------------------------------------------- 
 function ibadah_css_js_file_calling() {
-    wp_enqueue_style('bootstrap', get_template_directory_uri().'/assets/css/bootstrap.min.css');
-    wp_enqueue_style('bootstrap', get_template_directory_uri().'/assets/css/style.css');
-    wp_enqueue_style('owl-carousel', get_template_directory_uri().'/assets/css/owl.carousel.min.css');
-    wp_enqueue_style('owl-theme-default', get_template_directory_uri().'/assets/css/owl.theme.default.min.css');
-    wp_enqueue_style('nice-select', get_template_directory_uri().'/assets/css/nice-select.css');
-    wp_enqueue_style('fontawesome', get_template_directory_uri().'/assets/css/fontawesome.min.css');
-    wp_enqueue_style('style', get_stylesheet_uri());
+
+    wp_enqueue_style('ibadah_style', get_stylesheet_uri());
+    wp_register_style('bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css', array(), '5.3.2', 'all');
+    wp_enqueue_style('bootstrap');
+    wp_register_style('audioplayer', get_template_directory_uri() . '/assets/css/audioplayer.min.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('audioplayer');
+    wp_register_style('fontawesome', get_template_directory_uri() . '/assets/css/fontawesome.min.css', array(), '6.5.2', 'all');
+    wp_enqueue_style('fontawesome');
+    wp_register_style('jquery.fancybox', get_template_directory_uri() . '/assets/css/jquery.fancybox.min.css', array(), '1.0.1', 'all');
+    wp_enqueue_style('jquery.fancybox');
+    
+    // jQuery & js
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('bootstrap', get_template_directory_uri() . '/assets/js/bootstrap.bundle.min.js', array('jquery'), '5.3.2', true);
+
 }
-add_action( 'wp_enqueue_scripts', 'function ibadah_css_js_file_calling' );
+add_action( 'wp_enqueue_scripts', 'ibadah_css_js_file_calling' );
--- wp-content/themes/ibadah/index.php
+++ wp-content/themes/ibadah/index.php
@@ -1,7 +1,13 @@
+<?php
+/*
+* This templete for dispaying the header
+*/
+
+?>
 <!DOCTYPE html>
 <html lang="<?php language_attributes();?>" class="no-js">
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta charset="<?php bloginfo( 'charset' ) ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">  
     <?php wp_head(); ?>
